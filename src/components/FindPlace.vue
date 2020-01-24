3422cfd8ad1ed27f99951b25937bf1a4436e3eaf
Andriy Kashcha <anvaka@gmail.com>
2020-01-24 01:31:03 -0800

    added a note about 36e8

--- src/components/FindPlace.vue
+++ src/components/FindPlace.vue
@@ -129,6 +129,12 @@ export default {
             return {
               name: row.display_name,
               type: row.type,
+              // By convention the area id can be calculated from an existing 
+              // OSM way by adding 2400000000 to its OSM id, or in case of a 
+              // relation by adding 3600000000 respectively. So we are adding this
+              // https://wiki.openstreetmap.org/wiki/Overpass_API/Overpass_QL#By_area_.28area.29
+              // Note: we may want to do another case for osm_type = 'way'. Need to check
+              // if it returns correct values.
               areaId: row.osm_id + 36e8
             };
           } else if (row.boundingbox) {
