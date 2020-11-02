5121efab708278542271c25cc7b541cfa409cfd3
Andriy Kashcha <anvaka@gmail.com>
2020-11-02 09:00:04 -0800

    use s3 for caching instead of github
    Seems like I cannot update the github pages anymore.
    
    Switching to S3 - hope it wouldn't be too expensive to host this

--- src/config.js
+++ src/config.js
@@ -4,8 +4,11 @@ export default {
   /**
    * This is our caching backend
    */
-  areaServer: 'https://anvaka.github.io/index-large-cities/data',
-  // areaServer: 'http://localhost:8085', // This is un-commented when I develop cache locally
+  // This used to work, but seems like GitHub no longer allows large website hosting:
+  //areaServer: 'https://anvaka.github.io/index-large-cities/data',
+  //areaServer: 'http://localhost:8085', // This is un-commented when I develop cache locally
+  // So, using S3
+  areaServer: 'https://city-roads.s3-us-west-2.amazonaws.com/nov-02-2020',
 
   getDefaultLineColor() {
     return tinycolor('rgba(26, 26, 26, 0.8)');
